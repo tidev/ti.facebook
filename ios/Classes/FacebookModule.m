0ed4a3e64acb0a77c3580d2526dfb7d4d52b997c
Trey Jones <trey@eyesoreinc.com>
2016-04-11 16:23:24 -0400

    Reverted helper method used to set loginBehavior. Made some corrections to documentation.

--- ios/Classes/FacebookModule.m
+++ ios/Classes/FacebookModule.m
@@ -348,7 +348,7 @@ NSDictionary *launchOptions = nil;
  */
 -(void)setLoginBehavior:(id)arg
 {
-    NUMINT(arg);
+    ENSURE_TYPE(arg, NSNumber);
     
     loginBehavior = [arg unsignedIntegerValue];
 }
@@ -391,6 +391,7 @@ NSDictionary *launchOptions = nil;
     BOOL allowUI = args == nil ? YES : NO;
     NSArray *permissions_ = permissions == nil ? [NSArray array] : permissions;
     FBSDKLoginManager *loginManager = [[[FBSDKLoginManager alloc] init] autorelease];
+    [loginManager setLoginBehavior:FBSDKLoginBehaviorBrowser];
     if (loginBehavior) {
         [loginManager setLoginBehavior:loginBehavior];
     }
