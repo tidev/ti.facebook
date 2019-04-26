53b4385ee9c1a53fa55a6c192d1d0dcc36bb8e08
Chris Williams <chris.a.williams@gmail.com>
2019-04-26 13:12:20 -0400

    test: update assertions based on docs

--- test/unit/specs/constants.spec.js
+++ test/unit/specs/constants.spec.js
@@ -62,49 +62,45 @@ describe('ti.facebook', function () {
 			});
 		});
 
-		describe('BUTTON_STYLE_*', () => {
-			it('BUTTON_STYLE_NORMAL', () => {
-				expect(Facebook.BUTTON_STYLE_NORMAL).toEqual(jasmine.any(Number));
-			});
+		// Removed in 4.0.0?
+		// describe('BUTTON_STYLE_*', () => {
+		// 	it('BUTTON_STYLE_NORMAL', () => {
+		// 		expect(Facebook.BUTTON_STYLE_NORMAL).toEqual(jasmine.any(Number));
+		// 	});
 
-			it('BUTTON_STYLE_WIDE', () => {
-				expect(Facebook.BUTTON_STYLE_WIDE).toEqual(jasmine.any(Number));
-			});
-		});
+		// 	it('BUTTON_STYLE_WIDE', () => {
+		// 		expect(Facebook.BUTTON_STYLE_WIDE).toEqual(jasmine.any(Number));
+		// 	});
+		// });
 
-		describe('PLACE_LOCATION_CONFIDENCE_*', () => {
-			it('PLACE_LOCATION_CONFIDENCE_NOT_APPLICABLE', () => {
-				expect(Facebook.PLACE_LOCATION_CONFIDENCE_NOT_APPLICABLE).toEqual(jasmine.any(Number));
-			});
+		if (IOS) { // iOS-specific constants
+			describe('PLACE_LOCATION_CONFIDENCE_*', () => {
+				it('PLACE_LOCATION_CONFIDENCE_NOT_APPLICABLE', () => {
+					expect(Facebook.PLACE_LOCATION_CONFIDENCE_NOT_APPLICABLE).toEqual(jasmine.any(Number));
+				});
 
-			it('PLACE_LOCATION_CONFIDENCE_LOW', () => {
-				expect(Facebook.PLACE_LOCATION_CONFIDENCE_LOW).toEqual(jasmine.any(Number));
-			});
+				it('PLACE_LOCATION_CONFIDENCE_LOW', () => {
+					expect(Facebook.PLACE_LOCATION_CONFIDENCE_LOW).toEqual(jasmine.any(Number));
+				});
 
-			it('PLACE_LOCATION_CONFIDENCE_MEDIUM', () => {
-				expect(Facebook.PLACE_LOCATION_CONFIDENCE_MEDIUM).toEqual(jasmine.any(Number));
-			});
+				it('PLACE_LOCATION_CONFIDENCE_MEDIUM', () => {
+					expect(Facebook.PLACE_LOCATION_CONFIDENCE_MEDIUM).toEqual(jasmine.any(Number));
+				});
 
-			it('PLACE_LOCATION_CONFIDENCE_HIGH', () => {
-				expect(Facebook.PLACE_LOCATION_CONFIDENCE_HIGH).toEqual(jasmine.any(Number));
+				it('PLACE_LOCATION_CONFIDENCE_HIGH', () => {
+					expect(Facebook.PLACE_LOCATION_CONFIDENCE_HIGH).toEqual(jasmine.any(Number));
+				});
 			});
-		});
+		}
 
 		describe('LOGIN_BEHAVIOR_*', () => {
+			// FIXME: On Android these are Strings, but in docs/iOS they're numbers!
 			it('LOGIN_BEHAVIOR_BROWSER', () => {
 				expect(Facebook.LOGIN_BEHAVIOR_BROWSER).toEqual(jasmine.any(Number));
 			});
 
 			it('LOGIN_BEHAVIOR_NATIVE', () => {
 				expect(Facebook.LOGIN_BEHAVIOR_NATIVE).toEqual(jasmine.any(Number));
-			});
-
-			it('LOGIN_BEHAVIOR_SYSTEM_ACCOUNT', () => {
-				expect(Facebook.LOGIN_BEHAVIOR_SYSTEM_ACCOUNT).toEqual(jasmine.any(Number));
-			});
-
-			it('LOGIN_BEHAVIOR_WEB', () => {
-				expect(Facebook.LOGIN_BEHAVIOR_WEB).toEqual(jasmine.any(Number));
 			});
 
 			it('LOGIN_BEHAVIOR_NATIVE_WITH_FALLBACK', () => {
@@ -114,10 +110,21 @@ describe('ti.facebook', function () {
 			it('LOGIN_BEHAVIOR_DEVICE_AUTH', () => {
 				expect(Facebook.LOGIN_BEHAVIOR_DEVICE_AUTH).toEqual(jasmine.any(Number));
 			});
+
+			if (IOS) {
+				it('LOGIN_BEHAVIOR_SYSTEM_ACCOUNT', () => {
+					expect(Facebook.LOGIN_BEHAVIOR_SYSTEM_ACCOUNT).toEqual(jasmine.any(Number));
+				});
+
+				it('LOGIN_BEHAVIOR_WEB', () => {
+					expect(Facebook.LOGIN_BEHAVIOR_WEB).toEqual(jasmine.any(Number));
+				});
+			}
 		});
 
 		// FIXME: How does this interact with LOGIN_BEHAVIOR_* constants?
 		// FIXME: This points to a LoginButton.sessionLoginBehavior property not documented!
+		// FIXME: These are android only and do not appear to be around any more? Update docs?
 		describe('SSO_*', () => {
 			it('SSO_WITH_FALLBACK', () => {
 				expect(Facebook.SSO_WITH_FALLBACK).toEqual(jasmine.any(Number));
@@ -141,50 +148,54 @@ describe('ti.facebook', function () {
 				expect(Facebook.SHARE_DIALOG_MODE_NATIVE).toEqual(jasmine.any(Number));
 			});
 
-			it('SHARE_DIALOG_MODE_SHARE_SHEET', () => {
-				expect(Facebook.SHARE_DIALOG_MODE_SHARE_SHEET).toEqual(jasmine.any(Number));
-			});
-
-			it('SHARE_DIALOG_MODE_BROWSER', () => {
-				expect(Facebook.SHARE_DIALOG_MODE_BROWSER).toEqual(jasmine.any(Number));
-			});
-
 			it('SHARE_DIALOG_MODE_WEB', () => {
 				expect(Facebook.SHARE_DIALOG_MODE_WEB).toEqual(jasmine.any(Number));
-			});
-
-			it('SHARE_DIALOG_MODE_FEED_BROWSER', () => {
-				expect(Facebook.SHARE_DIALOG_MODE_FEED_BROWSER).toEqual(jasmine.any(Number));
 			});
 
 			it('SHARE_DIALOG_MODE_FEED_WEB', () => {
 				expect(Facebook.SHARE_DIALOG_MODE_FEED_WEB).toEqual(jasmine.any(Number));
 			});
+
+			if (IOS) {
+				it('SHARE_DIALOG_MODE_SHARE_SHEET', () => {
+					expect(Facebook.SHARE_DIALOG_MODE_SHARE_SHEET).toEqual(jasmine.any(Number));
+				});
+	
+				it('SHARE_DIALOG_MODE_BROWSER', () => {
+					expect(Facebook.SHARE_DIALOG_MODE_BROWSER).toEqual(jasmine.any(Number));
+				});
+
+				it('SHARE_DIALOG_MODE_FEED_BROWSER', () => {
+					expect(Facebook.SHARE_DIALOG_MODE_FEED_BROWSER).toEqual(jasmine.any(Number));
+				});
+			}
 		});
 
-		describe('MESSENGER_BUTTON_MODE_*', () => {
-			it('MESSENGER_BUTTON_MODE_CIRCULAR', () => {
-				expect(Facebook.MESSENGER_BUTTON_MODE_CIRCULAR).toEqual(jasmine.any(Number));
+		if (IOS) {
+			describe('MESSENGER_BUTTON_MODE_*', () => {
+				it('MESSENGER_BUTTON_MODE_CIRCULAR', () => {
+					expect(Facebook.MESSENGER_BUTTON_MODE_CIRCULAR).toEqual(jasmine.any(Number));
+				});
+
+				it('MESSENGER_BUTTON_MODE_RECTANGULAR', () => {
+					expect(Facebook.MESSENGER_BUTTON_MODE_RECTANGULAR).toEqual(jasmine.any(Number));
+				});
 			});
 
-			it('MESSENGER_BUTTON_MODE_RECTANGULAR', () => {
-				expect(Facebook.MESSENGER_BUTTON_MODE_RECTANGULAR).toEqual(jasmine.any(Number));
-			});
-		});
+			describe('MESSENGER_BUTTON_STYLE_*', () => {
+				it('MESSENGER_BUTTON_STYLE_BLUE', () => {
+					expect(Facebook.MESSENGER_BUTTON_STYLE_BLUE).toEqual(jasmine.any(Number));
+				});
 
-		describe('MESSENGER_BUTTON_STYLE_*', () => {
-			it('MESSENGER_BUTTON_STYLE_BLUE', () => {
-				expect(Facebook.MESSENGER_BUTTON_STYLE_BLUE).toEqual(jasmine.any(Number));
-			});
+				it('MESSENGER_BUTTON_STYLE_WHITE', () => {
+					expect(Facebook.MESSENGER_BUTTON_STYLE_WHITE).toEqual(jasmine.any(Number));
+				});
 
-			it('MESSENGER_BUTTON_STYLE_WHITE', () => {
-				expect(Facebook.MESSENGER_BUTTON_STYLE_WHITE).toEqual(jasmine.any(Number));
+				it('MESSENGER_BUTTON_STYLE_WHITE_BORDERED', () => {
+					expect(Facebook.MESSENGER_BUTTON_STYLE_WHITE_BORDERED).toEqual(jasmine.any(Number));
+				});
 			});
-
-			it('MESSENGER_BUTTON_STYLE_WHITE_BORDERED', () => {
-				expect(Facebook.MESSENGER_BUTTON_STYLE_WHITE_BORDERED).toEqual(jasmine.any(Number));
-			});
-		});
+		}
 
 		describe('LOGIN_BUTTON_TOOLTIP_BEHAVIOR_*', () => {
 			it('LOGIN_BUTTON_TOOLTIP_BEHAVIOR_AUTOMATIC', () => {
@@ -201,7 +212,8 @@ describe('ti.facebook', function () {
 		});
 
 		describe('LOGIN_BUTTON_TOOLTIP_STYLE_*', () => {
-			it('MESSENGER_BUTTON_STYLE_LOGIN_BUTTON_TOOLTIP_STYLE_NEUTRAL_GRAYBLUE', () => {
+			// FIXME: On Android these are Strings, but in docs/iOS they're numbers!
+			it('LOGIN_BUTTON_TOOLTIP_STYLE_NEUTRAL_GRAY', () => {
 				expect(Facebook.LOGIN_BUTTON_TOOLTIP_STYLE_NEUTRAL_GRAY).toEqual(jasmine.any(Number));
 			});
 
