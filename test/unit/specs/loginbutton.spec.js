0126681a22534a22db417c427252739f0a6122d3
Chris Williams <chris.a.williams@gmail.com>
2019-04-26 14:28:04 -0400

    test: add tests for LoginButton

---
+++ test/unit/specs/loginbutton.spec.js
@@ -0,0 +1,59 @@
+
+const Facebook = require('facebook');
+
+const IOS = (Ti.Platform.osname === 'iphone' || Ti.Platform.osname === 'ipad');
+const ANDROID = (Ti.Platform.osname === 'android');
+
+describe('ti.facebook', () => {
+	describe('LoginButton', () => {
+		let button;
+
+		beforeEach(() => {
+			button = Facebook.createLoginButton();
+		});
+
+		it('.apiName', () => {
+			expect(button.apiName).toBe('Ti.Facebook.LoginButton');
+		});
+
+		describe('properties', () => {
+			describe('.audience', () => {
+				it('defaults to AUDIENCE_ONLY_ME', () => {
+					expect(button.audience).toEqual(Facebook.AUDIENCE_ONLY_ME);
+				});
+				// TODO: change the value, verify it updates? try invalid values
+			});
+
+			describe('.publishPermissions', () => {
+				it('is a property', () => {
+					expect(Object.getOwnPropertyDescriptor(button, 'publishPermissions')).toBeDefined();
+				});
+				// TODO: change the value, verify it updates? try invalid values
+			});
+
+			describe('.readPermissions', () => {
+				it('is a property', () => {
+					expect(Object.getOwnPropertyDescriptor(button, 'readPermissions')).toBeDefined();
+				});
+				// TODO: change the value, verify it updates? try invalid values
+			});
+
+			describe('.tooltipBehavior', () => {
+				it('defaults to LOGIN_BUTTON_TOOLTIP_BEHAVIOR_AUTOMATIC', () => {
+					expect(button.tooltipBehavior).toEqual(Facebook.LOGIN_BUTTON_TOOLTIP_BEHAVIOR_AUTOMATIC);
+				});
+				// TODO: change the value, verify it updates? try invalid values
+			});
+
+			describe('.tooltipColorStyle', () => {
+				// should it default to friendly blue or neutral gray?
+				// Android Facebook docs say blue is default,
+				// I think enum defaults to blue on iOS too
+				it('defaults to LOGIN_BUTTON_TOOLTIP_STYLE_FRIENDLY_BLUE', () => {
+					expect(button.tooltipColorStyle).toEqual(Facebook.LOGIN_BUTTON_TOOLTIP_STYLE_FRIENDLY_BLUE);
+				});
+				// TODO: change the value, verify it updates? try invalid values
+			});
+		});
+	});
+});
