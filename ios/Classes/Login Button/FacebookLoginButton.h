a250217a8300a099525d0365865df35c15a3f901
vijay <vsingh@axway.com>
2019-10-18 12:23:09 -0700

    fix(ios): moved getters inside proxy to get actually called

--- ios/Classes/Login Button/FacebookLoginButton.h
+++ ios/Classes/Login Button/FacebookLoginButton.h
@@ -19,45 +19,27 @@ NS_ASSUME_NONNULL_BEGIN
   FBSDKLoginButton *_loginButton;
 }
 
+- (FBSDKLoginButton *)loginButton;
+
 /*!
  @brief Requested permissions when logging in. If set, do not set read permissions.
 */
 - (void)setPublishPermissions_:(NSArray<NSString *> *_Nonnull)publishPermissions;
-
-/*!
- @brief Returns the granted publish-permissions.
- */
-- (NSArray *_Nullable)publishPermissions;
 
 /*! 
  @brief Requested permissions when logging in. If set, do not set publish permissions.
  */
 - (void)setReadPermissions_:(NSArray<NSString *> *_Nullable)readPermissions;
 
-/*! 
- @brief Returns the granted read-permissions.
- */
-- (NSArray *_Nullable)readPermissions;
-
 /*!
  @brief The permissions to request.
  */
 - (void)setPermissions_:(NSArray<NSString *> *_Nullable)permissions;
 
-/*!
- @brief The permissions to request.
- */
-- (NSArray *_Nullable)permissions;
-
 /*! 
  @brief The default is AUDIENCE_ONLY_ME, only applicable to publish permissions.
  */
 - (void)setAudience_:(NSNumber *_Nonnull)audience;
-
-/*! 
- @brief The default audience to use, if publish permissions are requested at login time.
- */
-- (NSNumber *_Nonnull)audience;
 
 /*!
  @brief Returns the desired tooltip behavior.
@@ -65,19 +47,9 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)setTooltipBehavior_:(NSNumber *_Nonnull)tooltipBehavior;
 
 /*!
- @brief Returns the desired tooltip behavior.
- */
-- (NSNumber *_Nonnull)tooltipBehavior;
-
-/*!
  @brief Sets the desired tooltip color style.
  */
 - (void)setTooltipColorStyle_:(NSNumber *_Nonnull)tooltipColorStyle;
-
-/*!
- @brief Returns the desired tooltip color style.
- */
-- (NSNumber *_Nonnull)tooltipColorStyle;
 
 @end
 
