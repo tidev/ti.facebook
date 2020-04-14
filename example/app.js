1dc98ec7628a9804c0c85b2c07f50c41fd5f2b59
Fasihul Haque <fahaque@axway.com>
2020-04-14 14:50:54 +0530

    fix(ios): removed place and messenger support

--- example/app.js
+++ example/app.js
@@ -19,17 +19,6 @@ tabGroup.addTab(Titanium.UI.createTab({
 	window: require('facebook_share').window()
 }));
 
-if (Ti.Platform.osname !== 'android') {
-	tabGroup.addTab(Titanium.UI.createTab({
-		title: 'Places',
-		window: require('facebook_places').window()
-	}));
-	tabGroup.addTab(Titanium.UI.createTab({
-		title: 'Messenger',
-		window: require('facebook_messenger').window()
-	}));
-}
-
 fb.initialize(); // after you set up login/logout listeners and permissions
 
 // open tab group
