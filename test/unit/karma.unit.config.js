b6747b4bd08b8800e6c1e7f762264f113c863cc7
Fasihul Haque <fahaque@axway.com>
2020-04-28 20:14:14 +0530

    test: use 9.0.0.GA by default (#121)

--- test/unit/karma.unit.config.js
+++ test/unit/karma.unit.config.js
@@ -49,7 +49,7 @@ module.exports = config => {
 			}
 		],
 		titanium: {
-			sdkVersion: config.sdkVersion || '8.1.0.GA'
+			sdkVersion: config.sdkVersion || '9.0.0.GA'
 		},
 		customLaunchers: {
 			android: {
