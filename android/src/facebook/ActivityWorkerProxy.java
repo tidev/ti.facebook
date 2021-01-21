42268e9d8393975d1456269ab9deae2063b09873
Gary Mathews <contact@garymathews.com>
2021-01-21 14:47:46 -0800

    fix(android): init with lifecycleContainer

--- android/src/facebook/ActivityWorkerProxy.java
+++ android/src/facebook/ActivityWorkerProxy.java
@@ -4,7 +4,7 @@
  * Please see the LICENSE included with this distribution for details.
  *
  * Appcelerator Titanium Mobile
- * Copyright (c) 2015 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2020 by Axway, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  *
@@ -13,7 +13,6 @@ package facebook;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import com.facebook.*;
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollProxy;
@@ -21,73 +20,66 @@ import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.titanium.TiBaseActivity;
 import org.appcelerator.titanium.TiLifecycle.OnActivityResultEvent;
-import org.appcelerator.titanium.TiLifecycle.OnInstanceStateEvent;
 
 @Kroll.proxy(creatableInModule = TiFacebookModule.class)
-public class ActivityWorkerProxy extends KrollProxy implements OnActivityResultEvent, OnInstanceStateEvent
+public class ActivityWorkerProxy extends KrollProxy implements OnActivityResultEvent
 {
 	private static final String TAG = "ActivityWorkerProxy";
-	AccessTokenTracker accessTokenTracker;
 
-	// Constructor
+	private AccessTokenTracker accessTokenTracker;
+
 	public ActivityWorkerProxy()
 	{
 		super();
 	}
 
-	// Handle creation options
 	@Override
 	public void handleCreationDict(KrollDict options)
 	{
 		super.handleCreationDict(options);
-	}
 
-	@Override
-	public void onCreate(Activity activity, Bundle savedInstanceState)
-	{
-		Log.d(TAG, "onCreate called for ActivityWorkerProxy");
-		((TiBaseActivity) activity).addOnActivityResultListener(this);
-		accessTokenTracker = new AccessTokenTracker() {
-			@Override
-			protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
-			{
-				if (currentAccessToken == null) {
-					// User logged out
-					TiFacebookModule.getFacebookModule().fireEvent(TiFacebookModule.EVENT_LOGOUT, null);
-				} else {
-					// AccessToken refreshed
-					if (TiFacebookModule.getFacebookModule().isAccessTokenRefreshCalled()) {
-						TiFacebookModule.getFacebookModule().setAccessTokenRefreshCalled(false);
-						TiFacebookModule.getFacebookModule().fireEvent(TiFacebookModule.EVENT_TOKEN_UPDATED, null);
+		// If `lifecycleContainer` was set, this will be our proxy activity.
+		final Activity activity = getActivity();
+
+		if (activity instanceof TiBaseActivity) {
+			final TiBaseActivity baseActivity = (TiBaseActivity) activity;
+
+			// Obtain activity result.
+			baseActivity.addOnActivityResultListener(this);
+
+			// Create access token tracker.
+			accessTokenTracker = new AccessTokenTracker() {
+				@Override
+				protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
+				{
+					if (currentAccessToken == null) {
+
+						// User logged out.
+						TiFacebookModule.getFacebookModule().fireEvent(TiFacebookModule.EVENT_LOGOUT, null);
+					} else {
+
+						// AccessToken updated.
+						if (TiFacebookModule.getFacebookModule().isAccessTokenRefreshCalled()) {
+							TiFacebookModule.getFacebookModule().setAccessTokenRefreshCalled(false);
+							TiFacebookModule.getFacebookModule().fireEvent(TiFacebookModule.EVENT_TOKEN_UPDATED, null);
+						}
 					}
 				}
-			}
-		};
-	}
-
-	@Override
-	public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data)
-	{
-		Log.d(TAG, "ActivityWorkerProxy onActivityResult");
-		TiFacebookModule.getFacebookModule().getCallbackManager().onActivityResult(requestCode, resultCode, data);
+			};
+		}
 	}
 
 	@Override
 	public void onDestroy(Activity activity)
 	{
-		Log.d(TAG, "ActivityWorkerProxy onDestroy");
+		Log.d(TAG, "onDestroy");
 		accessTokenTracker.stopTracking();
 	}
 
 	@Override
-	public void onSaveInstanceState(Bundle outState)
+	public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data)
 	{
-		Log.d(TAG, "ActivityWorkerProxy onSaveInstanceState");
-	}
-
-	@Override
-	public void onRestoreInstanceState(Bundle inState)
-	{
-		Log.d(TAG, "ActivityWorkerProxy onRestoreInstanceState");
+		Log.d(TAG, "onActivityResult");
+		TiFacebookModule.getFacebookModule().getCallbackManager().onActivityResult(requestCode, resultCode, data);
 	}
 }
