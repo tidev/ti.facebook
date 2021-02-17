870026c11d90197b3c5c4555d1e28d2cb991290e
Christopher Williams <chris.a.williams@gmail.com>
2021-02-17 14:46:08 -0500

    test: use sdk 9.3.2.GA to test by default

--- test/unit/karma.unit.config.js
+++ test/unit/karma.unit.config.js
@@ -49,7 +49,7 @@ module.exports = config => {
 			}
 		],
 		titanium: {
-			sdkVersion: config.sdkVersion || '9.2.0.GA'
+			sdkVersion: config.sdkVersion || '9.3.2.GA'
 		},
 		customLaunchers: {
 			android: {
